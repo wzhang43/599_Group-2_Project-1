90d9deb823e448741a4f2f97d2d846693d7cb02d
wzhang43 <zhangwa@stat.oregonstate.edu>
2014-04-14 17:15:45 -0700

    Added code for producing bar chart and bar chart panels.

--- Fuel.R
+++ Fuel.R
@@ -38,7 +38,7 @@ library(ggplot2)
 ##  9. No fuel used
 
 
-
+######################################################################################
 
 
 ##### Read data #####
@@ -51,11 +51,40 @@ str(or_sm)
 or_df = tbl_df(or_sm)
 dim(or_df)
 
+which(names(or_df) %in% c("HFL","BLD"))
+
 
 ##### Subset data frame into variables of interest #####
 or_fuel = select(or_df, HFL,BLD)
 
+HFL_code = c("1"="1.Utility gas",
+             "2"="2.Bottled, tank or LP gas",
+             "3"="3.Electricity",
+             "4"="4.Fuel oil, kerosene, etc.",
+             "5"="5.Coal or coke",
+             "6"="6.Wood",
+             "7"="7.Solar energy",
+             "8"="8.Other fuel",
+             "9"="9.No fuel used")
+
+BLD_code = c("1"="Trailer",
+             "2"="1-family detached",
+             "3"="1-family attached",
+             "4"="2 APT",
+             "5"="3-4 APT",
+             "6"="5-9 APT",
+             "7"="10-19 APT",
+             "8"="20-49 APT",
+             "9"=">50 APT",
+             "10"="Boat/RV/van")
+
+or_fuel = mutate(or_fuel, fuel_type=HFL_code[as.character(HFL)],
+                 house_type=BLD_code[as.character(BLD)])
+
 ## We'll use or_fuel as the working dataframe from this point on.
+
+
+######################################################################################
 
 
 ##### Extracting information #####
@@ -64,23 +93,99 @@ or_fuel = select(or_df, HFL,BLD)
 ## within each type of housing unit, first group the data by the variable
 ## BLD (units in structure):
 
-house = group_by(or_fuel, BLD)
+(house = group_by(or_fuel,house_type))
 
 
-summarise(house, house_count=n())
+house_sum = house %.% 
+  summarise(house_count=n(),
+          type1=sum(HFL==1,na.rm=TRUE),
+          type2=sum(HFL==2,na.rm=TRUE),
+          type3=sum(HFL==3,na.rm=TRUE),
+          type4=sum(HFL==4,na.rm=TRUE),
+          type5=sum(HFL==5,na.rm=TRUE),
+          type6=sum(HFL==6,na.rm=TRUE),
+          type7=sum(HFL==7,na.rm=TRUE),
+          type8=sum(HFL==8,na.rm=TRUE),
+          type9=sum(HFL==9,na.rm=TRUE),
+          prop1=type1/house_count,
+          prop2=type2/house_count,
+          prop3=type3/house_count,
+          prop4=type4/house_count,
+          prop5=type5/house_count,
+          prop6=type6/house_count,
+          prop7=type7/house_count,
+          prop8=type8/house_count,
+          prop9=type9/house_count     
+          ) %.%
+  filter(house_type!="NA")
+
+## Retain only the proportions ##
+house_prop = select(house_sum,prop1:prop9)
 
 
+######################################################################################
 
 
+## Bar chart for distribution of housing unit types ##
+d <- data.frame(Units_in_structure=as.character(BLD_code), Count=house_sum$house_count)
+
+# Create side-by-side bar chart #
+require(ggplot2)
+ggplot(d, aes(x=Units_in_structure, y=Count)) + geom_bar(position=position_dodge())
+
+# Observation: 
+# (1) Not surprisingly, one-family detached home takes up the largest proportion of all types of homes in Oregon.
+#
+# (2) The second most type of housing unit is trailer/mobile home.
+#
+# (3) Originally I thought there would be more RV/van residents
 
 
+######################################################################################
 
 
+## Side-by-side bar chart for fuel types within housing unit types ##
+# x-axis labels for housing unit types #
+column1 <- c(rep("Trailer", 9), 
+             rep("1-family detached", 9),
+             rep("1-family attached", 9),
+             rep("2 APT", 9),
+             rep("2-4 APT", 9),
+             rep("5-9 APT", 9),
+             rep("10-19 APT", 9),
+             rep("20-49 APT", 9),
+             rep(">50 APT", 9),
+             rep("Boat/RV/van", 9))
 
+# Within-group labels for fuel types #
+column2 <- gl(9,1,9,labels=as.character(HFL_code))
 
+# Vector of entries #
+# Convert data into a vector for graphing use #
+house_prop_vec = rep(0,90)
+for(i in 0:9){
+  j=9*i+1
+  k=9*i+9
+  l=i+1
+  house_prop_vec[j:k]=as.numeric(house_prop[l,])
+}
 
+column3 <- house_prop_vec
 
+fuel_label = as.character(rep(c(1:9),10))
 
+df <- data.frame(Units_in_structure=column1, House_heating_fuel=column2, Percentage=column3, Fuel_label=fuel_label)
 
+# Create bar chart panels, grouped by housing unit type #
+require(ggplot2)
+ggplot()+
+  facet_wrap(~Units_in_structure)+ # panels separated by housing unit types
+  geom_bar(mapping=aes(x=Fuel_label, y=Percentage, fill=House_heating_fuel),data=df)+
+  xlab("House heating fuel")+
+  ggtitle(bquote(atop(.("Percentages of types of heating fuels for each type of housing unit"), atop(italic(.("Oregon 2008-2012")), "")))) # add title & subtitle
 
-
+# Observations:
+# (1) For all but one type of home (one-family detached), electricity is the most favored by residents. Utility gas is most popular among owners of one-family detached homes.
+#
+# (2) For all but two types of home, utility gas is the second favorite type of heating fuel for home owners. For boat/RV/van owners, their second favorite is bottled/tank/LP gas, and for trailer residents, it's wood.
+ 
